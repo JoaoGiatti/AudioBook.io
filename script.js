67edb580eabf23b03097600ed9c44adc79a04309
JoaoGiatti <joaogiatti2006@gmail.com>
2024-01-29 22:43:59 -0300

    Adicionando Projeto Completo

---
+++ script.js
@@ -0,0 +1,61 @@
+const botaoPlayPause = document.getElementById('play-pause');
+const botaoAvancar = document.getElementById('proximo');
+const botaoVoltar = document.getElementById('anterior');
+const nomeCapitulo = document.getElementById('capitulo')
+const audioCapitulo = document.getElementById('audio-capitulo');
+
+const numeroCapitulos = 10;
+let taTocando = 0;
+let capituloAtual = 1;
+
+function tocarFaixa(){
+    audioCapitulo.play();
+    taTocando = 1
+    botaoPlayPause.classList.remove('bi-play-circle-fill')
+    botaoPlayPause.classList.add('bi-pause-circle-fill')
+}
+function pausarFaixa(){
+    audioCapitulo.pause();
+    taTocando = 0
+    botaoPlayPause.classList.remove('bi-pause-circle-fill')
+    botaoPlayPause.classList.add('bi-play-circle-fill')
+}
+
+function tocarOuPausar(){
+    if(taTocando === 0){
+        tocarFaixa();
+    } else {
+        pausarFaixa();
+    }
+}
+
+function trocarNomeFaixa(){
+    nomeCapitulo.innerText = 'Capítulo ' + capituloAtual;
+}
+
+function proximaFaixa(){
+    if(capituloAtual === numeroCapitulos){
+        capituloAtual = 1;
+    } else{
+        capituloAtual = capituloAtual + 1;
+    }
+
+    audioCapitulo.src = "./books/dom-casmurro/" + capituloAtual + ".mp3";
+    tocarFaixa();
+    trocarNomeFaixa();
+}
+function voltarFaixa(){
+    if(capituloAtual === 1){
+        capituloAtual = numeroCapitulos;
+    } else{
+        capituloAtual = capituloAtual -1;
+    }
+
+    audioCapitulo.src = "./books/dom-casmurro/" + capituloAtual + ".mp3";
+    tocarFaixa();
+    trocarNomeFaixa();
+}
+
+botaoPlayPause.addEventListener('click', tocarOuPausar);
+botaoAvancar.addEventListener('click', proximaFaixa)
+botaoVoltar.addEventListener('click', voltarFaixa)
